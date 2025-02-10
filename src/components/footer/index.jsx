96884fc30a9ff628c7e45611a2b578ebbb5973ee
crno <118383162+Crn0@users.noreply.github.com.>
2025-02-10 17:23:46 +0800

    build: add test id

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,7 +2,11 @@ import PropTypes from 'prop-types';
 import style from './css/index.module.css';
 
 export default function Footer({ children, styles }) {
-  return <footer className={`${styles} ${style.margin_top_auto} ${style.dark}`}>{children}</footer>;
+  return (
+    <footer data-testid='footer' className={`${styles} ${style.margin_top_auto} ${style.dark}`}>
+      {children}
+    </footer>
+  );
 }
 
 Footer.propTypes = {
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,11 @@ import PropTypes from 'prop-types';
 import style from './css/index.module.css';
 
 export default function Header({ children, styles = '' }) {
-  return <header className={`${styles} ${style.header_dark}`}>{children}</header>;
+  return (
+    <header data-testid='header' className={`${styles} ${style.header_dark}`}>
+      {children}
+    </header>
+  );
 }
 
 Header.propTypes = {
