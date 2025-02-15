e5758182ebc95ab859a4e28dcb29ddfefd176733
crno <118383162+Crn0@users.noreply.github.com.>
2025-02-15 18:44:05 +0800

    build(play): import coordinate finders

--- src/pages/play/helpers/index.js
+++ src/pages/play/helpers/index.js
@@ -2,10 +2,14 @@ import handleModalState from './handle-modal-state';
 import handleGuessAnwser from './handle-guess-anwser';
 import handleImageClick from './handle-image-click';
 import handleCloseModal from './handle-close-modal';
+import findNormalizedCoords from '../../../helpers/normalized-coordinates';
+import findBoundary from '../../../helpers/coordinate-boundary';
 
 export default {
   handleModalState,
   handleGuessAnwser,
   handleImageClick,
   handleCloseModal,
+  findNormalizedCoords,
+  findBoundary,
 };
